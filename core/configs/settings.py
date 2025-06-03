66230032a22c58f2359d9a46b6af4e874085db38
azimi4002 <azimi4002@gmail.com>
2025-06-03 23:12:50 +1000

    fix requirements.txt and settings ENV

--- core/configs/settings.py
+++ core/configs/settings.py
@@ -5,8 +5,9 @@ from pydantic_settings import BaseSettings
 class Settings(BaseSettings):
     DB_USER: str
     DB_PASSWORD: str
-    DB_URL: str
+    DB_HOST: str
     DB_PORT: str
+    DB_NAME: str
     STORAGE_ENDPOINT: str
     STORAGE_ACCESS_KEY: str
     STORAGE_SECRET_KEY: str
